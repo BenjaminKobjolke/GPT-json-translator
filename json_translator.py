8e5950cfc23e0660fdb7275fca238a08226172fa
Benjamin Kobjolke <b.kobjolke@xida.de>
2024-02-02 08:23:30 +0100

    FIX: will not try to translate keys anymore
    FIX: encoding errors like
    
    {
      "About": "\u00dcber"
    }
    
    will not save as
    
    {
      "About": "Über"
    }

--- json_translator.py
+++ json_translator.py
@@ -11,20 +11,31 @@ from config import API_KEY, SOURCE_PATH
 openai.api_key = API_KEY
 
 # Set up target languages for translation
-languages = ["it-IT", "en-US",
+
+languagesAll = ["it-IT", "en-US",
              "fr-FR", "es-ES", "de-DE", "pt-PT", "pt-BR", "nl-NL", "ru-RU", "pl-PL", "tr-TR", "zh-CN", "ja-JP", "ko-KR",
              "ar-AR", "hi-IN", "sv-SE", "no-NO", "fi-FI", "da-DK", "cs-CZ",
              "sk-SK", "hu-HU", "ro-RO", "uk-UA", "bg-BG", "hr-HR", "sr-SP", "sl-SI", "et-EE", "lv-LV", "lt-LT", "he-IL",
              "fa-IR", "ur-PK", "bn-IN", "ta-IN", "te-IN", "mr-IN", "ml-IN", "th-TH", "vi-VN"]
-# languages = ["de-DE"]
-
+languages = ["de-DE"]
+'''
+languages = ["it-IT", "en-US",
+             "fr-FR", "es-ES", "de-DE"]
+'''
 
 argument_file_path = None
+
+# for debug
+# get the current working directory
+print(os.getcwd())
+
+argument_file_path = os.getcwd() + "\\locales\\en.json"
 if len(sys.argv) > 1:
     # Argument is provided
-    argument_file_path = sys.argv[1]
+    argument_file_path = sys.argv[1
+    ]
 
-# Prompt user to enter the path to the input JSON file
+# PromptAll user to enter the path to the input JSON file
 if argument_file_path:
     input_path = argument_file_path
 elif (SOURCE_PATH):
@@ -42,15 +53,15 @@ if not os.path.exists(input_path):
 try:
     with open(input_path, "r") as f:
         source_json = json.load(f)
-    # Do something with source_json
+    # Do E_something with source_json
 except PermissionError:
     print(f"Permission denied: Unable to read the file {input_path}")
     exit()
 
 
 # Define function to translate text for a given target language
-def translate(target_language, rows_to_translate):
-    print(f"Translating to {target_language}...")
+def translate(target_lang, rows_to_translate):
+    print(f"Translating to {target_lang}...")
     # Call OpenAI API to translate text
     completion = openai.chat.completions.create(
         model="gpt-4-1106-preview",
@@ -58,11 +69,11 @@ def translate(target_language, rows_to_translate):
         messages=[
             {
                 "role": "system",
-                "content": "You are TranslatorGpt, a powerful language model designed for seamless translation of text across multiple languages. You have been trained on a vast corpus of linguistic data and possess a deep understanding of grammar, syntax, and vocabulary of every language in the world.",
+                "content": "You are TranslatorGpt, a powerful language model designed for seamless translation of text across multiple languages. You have been trained on a vast corpus of linguistic data and possess a deep understanding of grammar, syntax, and vocabulary of every language in the world. You only translate json values, not json keys.",
             },
             {
                 "role": "user",
-                "content": f"Translate the following JSON to {target_language}:\n\n{rows_to_translate}\n",
+                "content": f"Translate the following JSON to {target_lang}:\n\n{rows_to_translate}\n",
             },
         ],
     )
@@ -70,7 +81,7 @@ def translate(target_language, rows_to_translate):
     translated_json_str = completion.choices[0].message.content
     # only double quotes are allowed in JSON, so replace single quotes with double quotes
     # translated_json_str = translated_json_str.replace("'", '"')
-    print(f"Translation to {target_language} complete.")
+    print(f"Translation to {target_lang} complete.")
     # print(f"Translated JSON:\n{translated_json}\n")
     # convert the string into a json object
     translated_json = json.loads(translated_json_str)
@@ -121,8 +132,8 @@ for future in concurrent.futures.as_completed(future_to_language):
         output_json = {**filtered_existing_json, **translated_json}
 
         output_path = os.path.join(os.path.dirname(input_path), filename)
-        with open(output_path, "w") as f:
-            json.dump(output_json, f, indent=2)
+        with open(output_path, "w", encoding="utf-8") as f:
+            json.dump(output_json, f, indent=2, ensure_ascii=False)
         print(f"Output file saved as {output_path}")
     except Exception as e:
         print(f"Error occurred while translating to {target_language}: {e}")
