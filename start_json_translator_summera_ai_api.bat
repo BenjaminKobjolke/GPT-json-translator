5c344cd8dfcf26c72980b7d9159007e467f889b8
Benjamin Kobjolke <b.kobjolke@xida.de>
2024-04-29 08:56:22 +0200

    PYTHON: add requirements.txt

--- start_json_translator_summera_ai_api.bat
+++
@@ -1,2 +0,0 @@
-call python json_translator.py "D:\wamp64\www\ai-chat-api\data\lang\de.xida.ai\en.json"
-pause
--- start_json_translator_summera_ai_app.bat
+++
@@ -1,2 +0,0 @@
-call python json_translator.py "D:\GIT\Intern\ai-chat\static\locales\en.json"
-pause
